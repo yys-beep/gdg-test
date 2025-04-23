4565e805ca874e66710c86e5a8dd84ca9d2f3fb3
yys-beep <yysyuen123@gmail.com>
2025-04-23 17:25:07 +0800

    Update getweather.js

--- netlify/functions/getweather.js
+++ netlify/functions/getweather.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 exports.handler = async (event, context) => {
   const { latitude, longitude } = event.queryStringParameters;
